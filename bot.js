2de7ba6f6b9684ee768f9ac3b8f89822dda680bb
anasshadow <54816572+anasshadow@users.noreply.github.com>
2019-09-03 22:03:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("618535857279991872")
 setInterval(function() {
 channel.send(`الزعامة فوقك يمعرص خد ناحو hhhhhhh ksmk`);
-}, 40)
+}, 1000)
 });
 
 client.on('message', message => {
